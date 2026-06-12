d8530ba5fc026187482d28939fd7c38a04884f9a
mathmaxyz <h.hopecarter@gmail.com>
2026-06-12 23:57:53 +0100

    Initial commit:

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Max Hope-Carter</title>
+</svelte:head>
+
+{@render children()}
