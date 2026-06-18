4585eeacccde7eb0de823b521f3dbe7750c0fb2d
mathmaxyz <h.hopecarter@gmail.com>
2026-06-18 20:19:28 +0100

    Create about page with blurb

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,67 @@
+<div class="about-wrapper">
+	<img
+		alt="The future you get is the future you create"
+		class="about-image"
+		src="/th_future_you_get.jpg"
+	/>
+	<p class="post-excerpt">
+		My name is Max and I make things.
+
+		<br />
+		<br />
+
+		All I wanted to do since I was a kid was make a book or an album or a video game or a business
+		or a painting... Terrified of leaving nothing special behind after me.
+
+		<br />
+		<br />
+
+		I became a software engineer because I saw computers and the internet as the ultimate canvas, a
+		panacea of knowledge sharing, and a living, breathing community. Tech felt optimistic and
+		liberating, and the internet was a wave you surfed into the unknown, rather than a curated feed
+		you scrolled.
+
+		<br />
+		<br />
+
+		This website is an attempt to contribute to the "lower internet" or "<a
+			class="embed-link"
+			href="https://zsrobinson.com/posts/the-tiny-internet/">tiny internet</a
+		>". A place where people are radically creating a generous, curious and distributed internet
+		again.
+
+		<br />
+		<br />
+
+		It is a blog (yes, like its 2005), a showcase of my creations (click that big play button!!),
+		and it's full of easter eggs to rewards curiosity.
+
+		<br />
+		<br />
+
+		If you want to contact me for collaboration oppurtunities please go to my contact page. I am
+		very open to new work.
+
+		<br />
+		<br />
+
+		Thank you for coming to my corner of the internet.
+	</p>
+</div>
+
+<style>
+	.about-wrapper {
+		margin: 20px 0 20px 0;
+		display: flex;
+		align-items: flex-start;
+	}
+
+	.about-image {
+		width: 40%;
+		border-top: 50px solid var(--dark-green);
+		border-bottom: 20px solid var(--dark-green);
+		border-left: 10px solid var(--dark-green);
+		border-right: 10px solid var(--dark-green);
+		margin: 5px 30px 0 0;
+	}
+</style>
