04bfcec8107591ee6f0d87c15c63b0680f24f90b
mathmaxyz <h.hopecarter@gmail.com>
2026-07-18 19:17:51 +0100

    Remove minmax button from blog

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -7,17 +7,14 @@
 <ul class="post-list">
 	{#each data.posts as post, i}
 		<li>
-			<div class="topbar">
-				<div class="topbar-left">
-					<a class="post-link" href="/blog/{post.slug}">
+			<a href="/blog/{post.slug}">
+				<div class="topbar">
+					<div class="topbar-left">
 						<span class="topbar-heading">{post.title}</span>
-					</a>
-					<span class="meta-text">{post.date}</span>
+						<span class="meta-text">{post.date}</span>
+					</div>
 				</div>
-				<a class="post-link" href="/blog/{post.slug}">
-					<MinMaxButton></MinMaxButton>
-				</a>
-			</div>
+			</a>
 			<div class="post-teaser-content">
 				<img class="post-thumbnail" src={post.thumbnail} />
 				<p class="post-excerpt">{post.excerpt} ...</p>
